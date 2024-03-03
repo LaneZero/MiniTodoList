3d8eeff8114d628a527bafe848c835f25b7f8c50
LaneZero <Ahmad.Rahimi@live.com>
2024-03-03 03:52:58 +0330

    --Add Show Func --Add insert Func --Add remove and delete func

--- MiniTodoList/Program.cs
+++ MiniTodoList/Program.cs
@@ -1,2 +1,126 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+string userResponse ;
+List<string> userTasks = new List<string>();
+
+do
+{
+    cleanScreen();
+    mainMenu();
+    userResponse = Console.ReadLine().ToString();
+    switch(userResponse)
+    {
+        case "S":
+        case "s":
+        case "1":
+            showAllTasks();
+            break;
+
+        case "A":
+        case "a":
+        case "2":
+            insertTaskToList();
+            break;
+
+        case "R":
+        case "r":
+        case "3":
+            RemoveTaskFromListByTitle();
+            break;
+
+        case "D":
+        case "d":
+        case "4":
+            DeleteAllTasks();
+            break;
+
+        case "e":
+        case "E":
+        case "5":
+           
+            break;
+
+        default:
+            Console.WriteLine("Wrong input press any key for try agian,");
+            Console.ReadLine();
+            break;
+
+    }
+} while (userResponse != "5" && userResponse != "e" && userResponse != "E");
+
+
+//Menu
+void mainMenu()
+{
+    Console.WriteLine("1.[S]how All Todo Task.");
+    Console.WriteLine("2.[A]dd task to list.");
+    Console.WriteLine("3.[R]emove task with title.");
+       Console.WriteLine("4.[D]elete all tasks.");
+    Console.WriteLine("5.[E]xit.");
+    Console.WriteLine("--=---=---=-=--=-=-=-===---=-");
+    Console.Write("Choose your destiny:");
+
+}
+
+//clear Screen
+void cleanScreen()
+{
+    System.Console.Clear();
+}
+
+
+void showAllTasks()
+{
+    Console.WriteLine($"Your List have {userTasks.Count} Tasks.");
+    Console.WriteLine($"---------------------------------------");
+
+    foreach (string task in userTasks)
+    {
+        Console.WriteLine($"{task}");
+    }
+    Console.WriteLine($"---------------------------------------");
+    Console.ReadLine();
+}
+//insert to list
+void insertTaskToList()
+{
+    string utask;
+    Console.WriteLine("please Enter Your Task:");
+    utask=Console.ReadLine();
+    userTasks.Add(utask);
+    Console.WriteLine($"{utask} -> Successfully Added.");
+    Console.ReadLine();
+}
+
+// remove from list
+void RemoveTaskFromListByTitle()
+{
+    string utask;
+    showAllTasks();
+    Console.WriteLine("please Enter Your Task title:");
+    utask = Console.ReadLine();
+    if (userTasks.Remove(utask))
+    {
+        Console.WriteLine($"{utask} -> Successfully Removed.");
+    }
+    else
+    {
+        Console.WriteLine($"{utask} -> not found.");
+    }    
+    
+    Console.ReadLine();
+}
+
+void DeleteAllTasks()
+{
+    Console.WriteLine("Are you sure to delete All Tasks ? press Y");
+    if (Console.ReadLine()=="y")
+    {
+        userTasks.Clear();
+    }
+    else
+    {
+        Console.WriteLine("nothing remove");
+    }
+    Console.ReadLine();
+}
